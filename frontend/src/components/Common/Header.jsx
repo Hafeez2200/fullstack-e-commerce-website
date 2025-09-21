68f762d863558d544a32ac7881bcb7cce04b96b0
Hafeez Ahmed <hafeezahmad2200@gmail.com>
2025-09-21 17:33:12 +0300

    added search button and cart drawer

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <header>
+    <header className=' border-b border-gray-200'>
         {/* Top Bar */}
         <TopBar/>
         {/* Nav Bar */}
--- frontend/src/components/Common/NavBar.jsx
+++ frontend/src/components/Common/NavBar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HiOutlineUser,HiOutlineShoppingBag,HiBars3BottomRight } from "react-icons/hi2";
+import SearchBar from './SearchBar';
+import CartDrawer from '../Layout/CartDrawer';
 
 const NavBar = () => {
+
+    const [drawerOpen, setDrawerOpen] = useState(true);
+    
+
+  const toogleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
     <>
         <nav className=' container mx-auto flex items-center justify-between py-4 px-6'>
@@ -33,18 +43,23 @@ const NavBar = () => {
                 <Link to="/profile" className=' hover:text-black' >
                 <HiOutlineUser className=' h-6 w-6 text-gray-700' />
                 </Link>
-                <button className=' relative hover:text-black'>
+                <button onClick={toogleCartDrawer} className=' relative hover:text-black'>
                     <HiOutlineShoppingBag className=' h-6 w-6 text-gray-700' />
                     <span className=' absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
                 </button>
 
                 {/* Search Icon */}
+                <div className=' overflow-hidden'>
+                    <SearchBar/>
+                </div>
+                
                 
                 <button className='md:hidden'>
                     <HiBars3BottomRight className=' h-6 w-6 text-gray-700'/>
                 </button>
             </div>
         </nav>
+        <CartDrawer drawerOpen={drawerOpen} toogleCartDrawer={ toogleCartDrawer} />
     </>
   )
 }
