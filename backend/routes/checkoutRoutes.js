83675441acf2f8d6fcbd71ff94b39d6676dece65
Hafeez Ahmed <hafeezahmad2200@gmail.com>
2025-12-07 14:14:41 +0200

    completed checkout routes and order routes

--- backend/routes/checkoutRoutes.js
+++ backend/routes/checkoutRoutes.js
@@ -65,4 +65,52 @@ router.put("/:id/pay",protect, async(req,res)=>{
         console.error(error);
         res.status(500).json({message: "Server Error"});
     }
-})
+});
+
+// @route POST /api/checkout/:id/finalize
+// @desc finalize checkout and convert to an order after payment confirmation
+// @access Private
+router.post("/:id/finalize", protect, async (req,res)=>{
+    try {
+        const checkout = await Checkout.findById(req.params.id);
+
+        if(!checkout){
+            return res.status(404).json({message: "Checkout not found"});
+        }
+        
+        if(checkout.isPaid && !checkout.isFinalized){
+            // create final order based on the checkout details
+            const finalOrder = await Order.create({
+                user: checkout.user,
+                orderItem: checkout.checkoutItems,
+                shippingAddress: checkout.shippingAddress,
+                paymentMethod: checkout.paymentMethod,
+                totalPrice: checkout.totalPrice,
+                isPaid: true,
+                paidAt: checkout.paidAt,
+                isDelivered: false,
+                paymentStatus: "paid",
+                paymentDetails: checkout.paymentDetails,
+            });
+
+            // Mark the checkout as finalized
+            checkout.isFinalized = true;
+            checkout.finalizedAt = Date.now();
+            await checkout.save();
+            // Delete the cart associated with the user
+            await Cart.findOneAndDelete({user: checkout.user});
+            res.status(201).json(finalOrder);
+
+        } else if(checkout.isFinalized){
+            res.status(400).json({message: "Checkout is already finalized"});
+        } else {
+            res.status(400).json({message: "Checkout is not paid"});
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({messsage : "Server Error"});
+    }
+});
+
+module.exports = router;
